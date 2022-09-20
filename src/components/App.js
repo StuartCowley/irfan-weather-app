e2f8c28c9be3d69682766bb05e426b0637524608
Irfan Hussain <irfan185@gmail.com>
2022-09-20 07:06:46 +0100

    forecast summaries component

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,34 @@ import "../styles/App.css";
 import PropTypes from "prop-types";
 
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
+function App({ location, forecasts }) {
   const { city, country } = location;
 
   return (
     <div className="App">
       <LocationDetails city={city} country={country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
-  }).isRequired,
-  // do we need //
-  forecasts: PropTypes.shape({
-    date: PropTypes.number.isRequired,
-    description: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    temperature: PropTypes.shape({
-      min: PropTypes.number,
-      max: PropTypes.number,
-    }),
   }).isRequired,
 };
 
